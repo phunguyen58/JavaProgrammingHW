84d0f5e5f593e928b8969d31ee31a4c5761b7a7b
phuUS <nvphu20@clc.fitus.edu.vn>
2022-11-21 05:38:13 +0700

    First commit

---
+++ src/FileManagement.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class FileManagement {
+    public static void CountNumOfLines(String inputFileName){
+        int count = 0;
+
+        try {
+            // create a new file object
+            File file = new File(inputFileName);
+
+            // create an object of Scanner
+            // associated with the file
+            Scanner sc = new Scanner(file);
+
+            // read each line and
+            // count number of lines
+            while(sc.hasNextLine()) {
+                sc.nextLine();
+                count++;
+            }
+            System.out.println("Total Number of Lines: " + count);
+
+            // close scanner
+            sc.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void CountNumOfWords(String inputFileName) {
+        try{
+            // create a new file object
+            File file = new File(inputFileName);
+
+            // create an object of Scanner
+            // associated with the file
+            Scanner sc = new Scanner(file);
+            int count=0;
+            while(sc.hasNext()){
+                sc.next();
+                count++;
+            }
+            System.out.println("Number of words: " + count);
+        } catch (Exception e){
+            System.out.println(e);
+        }
+
+    }
+
+    public static void main(String []args){
+        CountNumOfLines("input.txt");
+        CountNumOfWords("input.txt");
+    }
+
+
+}
